f16e03c6d8e525e0f02a5d026057781262f19378
Abd0id <abdallahidfat@gmail.com>
2025-11-22 18:30:16 +0100

    fixing svg fill colors

--- dark-mode.js
+++ dark-mode.js
@@ -2,7 +2,6 @@ const html = document.documentElement;
 const themeToggle = document.getElementById("theme-toggle");
 const iconMoon = document.getElementById("theme-icon-moon");
 const iconSun = document.getElementById("theme-icon-sun");
-const iconProp = document.getElementById("theme-icon-prop");
 
 
 //apply saved theme on load
@@ -10,12 +9,10 @@ if (localStorage.theme === "light") {
   html.classList.remove("dark");
   iconMoon.classList.remove("hidden");
   iconSun.classList.add("hidden");
-  iconProp.setAttribute("fill", "#000000");
 } else {
   html.classList.add("dark");
   iconMoon.classList.add("hidden");
   iconSun.classList.remove("hidden");
-  iconProp.setAttribute("fill", "#FFFFFF");
 }
 
 //theme toggle handler
@@ -23,7 +20,6 @@ themeToggle.addEventListener("click", () => {
   const isDark = html.classList.toggle("dark");
   iconMoon.classList.toggle("hidden", isDark);
   iconSun.classList.toggle("hidden", !isDark);
-  iconProp.setAttribute("fill", isDark ? "#FFFFFF" : "#000000");
   localStorage.theme = isDark ? "dark" : "light";
 });
 
